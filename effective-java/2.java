748ab3dbc34ce3f6d8a66da195c9e2e5a5d3072a
mynspluto <mynspluto@gmail.com>
2024-03-10 23:44:50 +0900

    builder 작성중

--- effective-java/2.java
+++ effective-java/2.java
@@ -75,7 +75,7 @@ public class NutritionFacts2 {
       carbohydrate = val;
       return this;
     }
-    
+
   }
 
   private NutritionFacts2(Builder builder) {
@@ -87,3 +87,99 @@ public class NutritionFacts2 {
     this.carbohydrate = builder.carbohydrate;
   }
 }
+
+public abstract class Pizza {
+  public enum Topping {HAM, MUSHROOM, ONION, PEPPER, SAUSAGE}
+
+  final Set<Topping> toppings;
+
+  abstract static class Builder<T extends Builder<T>> {
+    EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
+
+    public T addToping(Topping topping) {
+      toppings.add(Objects.requireNonNull(topping));
+      return self();
+    }
+
+    abstract Pizza build();
+
+    protected abstract T self();
+  }
+
+  Pizza(Builder<?> builder) {
+    toppings = builder.toppings.clone();
+  }
+}
+
+public class NyPizza extends Pizza {
+
+  public enum Size {
+    SMALL, MEDIUM, LARGE
+  }
+
+  private final Size size;
+
+  public static class Builder extends Pizza.Builder<Builder> {
+    private final Size size;
+
+    public Builder(Size size) {
+      this.size = Objects.requireNonNull(size);
+    }
+
+
+    @Override
+    public NyPizza build() {
+      return new NyPizza(this);
+    }
+
+    @Override
+    protected Builder self() {
+      return this;
+    }
+  }
+
+  private NyPizza(Builder builder) {
+    super(builder);
+    size = builder.size;
+  }
+}
+
+public class Calzone extends Pizza {
+
+  private final boolean sauceInside;
+
+  public static class Builder extends Pizza.Builder<Builder> {
+    private boolean sauseInside = false;
+
+    public Builder sauceInde() {
+      sauseInside = true;
+      return this;
+    }
+
+    @Override
+    public Calzone build() {
+      return new Calzone(this);
+    }
+
+    @Override
+    protected Builder self() {
+      return this;
+    }
+  }
+
+  private Calzone(Builder builder) {
+    super(builder);
+    sauceInside = builder.sauseInside;
+  }
+
+}
+
+NyPizza nyPizza = new NyPizza.Builder(SMALL)
+    .addTopping(Pizza.Topping.SAUSAGE)
+    .addTopping(Pizza.Topping.ONION)
+    .build();
+
+Calzone calzone = new Calzone.Builder()
+    .addTopping(Pizza.Topping.HAM)
+    .sauceInde()
+    .build();
