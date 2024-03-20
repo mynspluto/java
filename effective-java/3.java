270e1720d72ba65879977c1688c414530e629e03
mynspluto <mynspluto@gmail.com>
2024-03-20 23:39:21 +0900

    finalizer와 cleaner 사용을 피하라

--- effective-java/3.java
+++ effective-java/3.java
@@ -1,22 +1,29 @@
 // private 생성자나 열거 타입으로 싱글턴임을 보증하라
+
+// public static final 필드 방식의 싱글턴
 public class Elvis {
 
   public static final Elvis INSTANCE = new Elvis();
-
   private Elvis() {}
+
+  public void leaveTheBuilding() { ... }
 }
 
-public class Evlis {
+// 정적 팩터리 방식의 싱글턴
+public class Elvis {
 
   private static final Elvis INSTANCE = new Elvis();
-
   private Elvis() {}
-
   public static Elvis getInstance() {
     return INSTANCE;
   }
+
+  public void leaveTheBuilding() { ... }
 }
 
+// 열거 타입 방식의 싱글턴
 public enum Elvis {
-  INSTANCE,
+  INSTANCE;
+
+  public void leaveTheBuilding() { ... }
 }
