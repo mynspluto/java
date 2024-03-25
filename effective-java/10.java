ebdc2e65baadcb9235d3fd2d8f41f76750503f6f
mynspluto <mynspluto@gmail.com>
2024-03-25 21:44:41 +0900

    추이성 위배 사례 추가

--- effective-java/10.java
+++ effective-java/10.java
@@ -1,3 +1,6 @@
+// equals는 일반 규약을 지켜 재정의하라
+
+// 대칭성 위배 x.equals(y) != y.equals(x)
 public final class CaseInsenstiveString {
 
   private final String s;
@@ -11,7 +14,21 @@ public final class CaseInsenstiveString {
     if (o instanceof CaseInsenstiveString) return s.equalsIgnoreCase(
       ((CaseInsenstiveString) o).s
     );
+    if (o instanceof String) return s.equalsIgnoreCase((String) o);
 
     return false;
   }
+}
+
+// 추이성 위배 null이 아닌 모든 참조 값 x, y에 대해 x.equals(y)가 true고 y.equals(z)도 true면 x.equals(z)도 true다
+@Override public boolean equals(Object o) {
+  if (!(o instanceof Point))
+    return false;
+  
+  // o가 일반 Point면 색상을 무시하고 비교한다.
+  if (!(o instanceof ColorPoint))
+    return o.equals(this);
+
+  // o가 ColorPoint면 색상까지 비교한다.
+  return super.euqlas(o) && ((ColorPoint) o).color == color;
 }
